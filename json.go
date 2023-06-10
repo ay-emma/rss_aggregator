4cfd38e4d748c6f4034f4006e2ec759a08d79328
whoisAy <adedokunemmanuel250@gmail.com>
2023-06-10 20:53:57 +0100

    feat: response message types abstrations and server set up

---
+++ json.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+func respondWithError(w http.ResponseWriter, code int, msg string){
+	if code > 499 {
+		log.Println("Responding with 5XX error:", msg)
+	}
+}
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+
+	dat, err := json.Marshal(payload)
+
+	if err != nil {
+		log.Printf("failed to marshal JSON respons %v", payload)
+		w.WriteHeader(500)
+
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
